cb3d19d863b5ca3a36d1c7b82fd797d76d34468f
williams <ikswaqqs40@gmail.com>
2022-06-02 12:21:57 +0100

    added image to projects

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,7 +13,7 @@ function Home() {
         <Container className="home-content">
           <Row>
             <Col md={7} className="home-header">
-              <h1 style={{ paddingBottom: 15 }} className="heading">
+              <h1 className="heading">
                   <span className="light-silver">Hi There!{" "}</span>
                 <span className="wave" role="img" aria-labelledby="wave">
                   ✋
