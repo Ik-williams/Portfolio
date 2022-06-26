08e5fa5f7689ebc30a9e9354df4aa779227358ee
williams <ikswaqqs40@gmail.com>
2022-06-26 02:09:59 +0100

    added a project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -37,7 +37,6 @@ function Projects() {
               link="https://github.com/Ik-williams/tourer-app"
             />
           </Col>
-
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={cuisine}
@@ -46,7 +45,6 @@ function Projects() {
               link="https://github.com/Ik-williams/cuisine-diner"
             />
           </Col>
-
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={covid}
